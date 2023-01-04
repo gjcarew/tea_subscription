95222951a7766c5b8a70ba4b5234d8221052a2bc
Gavin Carew <gjcarew@gmail.com>
2023-01-04 10:42:56 -0800

    Add tea model to subscriptions app

---
+++ subscriptions/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Subscription(models.Model):
+    title = models.CharField(max_length=100, blank=True, default='')
+    price = models.IntegerField()
+    active = models.BooleanField(help_text='Active status', default=True)
+    frequency = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Tea(models.Model):
+    subscription = models.ManyToManyField(Subscription)
+    title = models.CharField(max_length=100, blank=True, default='')
+    description = models.TextField()
+    temperature = models.IntegerField()
+    brew_time = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Customer(models.Model):
+    subscription = models.ManyToManyField(Subscription)
